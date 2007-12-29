7e9973075d0a6fded31a588d90b26a515fbfa61b
ritzalam <ritzalam@4d7d1a10-063c-0410-970e-99ecacbbc69c>
2007-12-29 22:48:58 +0000

    ch11

--- lib/string.rb
+++ lib/string.rb
@@ -3,4 +3,15 @@ class String
   def or_else(alternate)
     blank? ? alternate : self
   end
+  
+  # Capitalize each word (space separated).
+  def capitalize_each
+    space = " "
+    split(space).each{ |word| word.capitalize! }.join(space)
+  end
+  
+  # Capitalize each word in place.
+  def capitalize_each!
+    replace capitalize_each
+  end
 end
