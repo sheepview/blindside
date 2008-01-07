fbd77984330191c803e9b1406bf67ffe7917b265
ritzalam <ritzalam@4d7d1a10-063c-0410-970e-99ecacbbc69c>
2008-01-07 02:22:18 +0000

    ch13

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,6 +9,7 @@ class ProfileController < ApplicationController
     @hide_edit_links = true
     screen_name = params[:screen_name]
     @user = User.find_by_screen_name(screen_name)
+    @logged_in_user = User.find(session[:user_id]) if logged_in?
     if @user
       @title = "My RailsSpace Profile for #{screen_name}"
       @spec = @user.spec ||= Spec.new
