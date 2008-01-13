b1124a50fa345001c60fe41ec8a744a4be0e68b2
ritzalam <ritzalam@4d7d1a10-063c-0410-970e-99ecacbbc69c>
2008-01-13 03:46:48 +0000

    ch15

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :blogs do |blog|
+    blog.resources :posts
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   
   # Sample of regular route:
